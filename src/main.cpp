9c17f0b9a3f3e2bafaffbf84f2a272bccce24b48
Duncan Tilley <duncan.tilley@gmail.com>
2017-06-08 15:14:52 +0200

    Add color

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,39 @@ using std::string;
 
 #include "field.h"
 
+#define WHITE 1
+#define RED 2
+#define YELLOW 3
+#define GREEN 4
+#define BLUE 5
+#define RED_INVERSE 6
+
 // draw the field at the given y and x
 void drawField(Field* field, int y, int x, bool show) {
 	for (int r = 0; r < field->getRows(); r++) {
 		move(y + r, x);
 		for (int c = 0; c < field->getCols(); c++) {
-			addch(field->getChar(r, c, show));
+			char cur = field->getChar(r, c, show);
+			int color = 0;
+			// determine the color
+			if (cur == MINE_CHAR) {
+				color = RED_INVERSE;
+			} else if (cur == FLAG_CHAR) {
+				color = BLUE;
+			} else if (cur >= '0' && cur <= '9') {
+				if (cur >= '3') {
+					color = RED;
+				} else if (cur >= '2') {
+					color = YELLOW;
+				} else {
+					color = GREEN;
+				}
+			} else {
+				color = WHITE;
+			}
+			attron(COLOR_PAIR(color));
+			addch(cur);
+			attroff(COLOR_PAIR(color));
 			addch(' ');
 		}
 	}
@@ -46,6 +73,14 @@ int main() {
 	raw();
 	noecho();
 	keypad(stdscr, true);
+	// initialize some colors
+	start_color();
+	init_pair(WHITE, COLOR_WHITE, COLOR_BLACK);
+	init_pair(RED, COLOR_RED, COLOR_BLACK);
+	init_pair(YELLOW, COLOR_YELLOW, COLOR_BLACK);
+	init_pair(GREEN, COLOR_GREEN, COLOR_BLACK);
+	init_pair(BLUE, COLOR_BLUE, COLOR_BLACK);
+	init_pair(RED_INVERSE, COLOR_BLACK, COLOR_RED);
 	// TODO receive the field size and difficulty
 	int frows = 0, fcols = 0;
 	int currow = 0, curcol = 0;
